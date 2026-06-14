b5523013b817b70347a86acaa22570a9f9b6a81c
soathdh <tgsoat649@gmail.com>
2026-06-14 18:45:04 +0500

    main.py

--- main.py
+++ main.py
@@ -5,8 +5,6 @@ import pytz
 from telethon import TelegramClient
 
 # --- SOZLAMALAR ---
-# Bu yerga noutbukda ishlatgan api_id va api_hash larni yozishingiz shart emas, 
-# chunki allaqachon noutbukda yaratilgan .session fayli orqali kiramiz.
 API_ID = 123456  
 API_HASH = 'change_this_or_keep'
 
@@ -16,8 +14,7 @@ TZ = pytz.timezone('Asia/Tashkent')
 logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
 
 async def main():
-    # 'session_name' nomi noutbukdagi .session fayli nomi bilan bir xil bo'lishi kerak.
-    # Agar noutbukda fayl nomi 'session_name.session' bo'lsa, bu yerga 'session_name' yoziladi.
+    # Siz yuklagan fayl nomi clock_session bo'lgani uchun shu nom qoladi
     client = TelegramClient('clock_session', API_ID, API_HASH)
     
     await client.start()
@@ -33,7 +30,7 @@ async def main():
             
             # Agar daqiqa o'zgargan bo'lsa, nikni yangilash
             if current_time != last_time:
-                new_first_name = f"FEIN | {current_time}" # 'YourName' o'rniga o'z nikingizni yozing
+                new_first_name = f"FEIN | {current_time}"
                 
                 # Profilni yangilash
                 from telethon.tl.functions.account import UpdateProfileRequest
